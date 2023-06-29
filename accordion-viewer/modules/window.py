4b07f9df8d59b908173b5defd240c07bf88b8175
Fabian Voigt <fvoigt@fas.harvard.edu>
2023-06-29 14:12:03 -0400

    Added pyqtgraph scatterplot

--- accordion-viewer/modules/window.py
+++ accordion-viewer/modules/window.py
@@ -7,6 +7,13 @@ import qdarkstyle
 from .worker import WorkerObject, AnotherWorkerObject
 from .state import SingletonStateObject
 
+import pyqtgraph as pg
+import numpy as np
+
+from metavision_core.event_io.raw_reader import RawReader
+from metavision_core.event_io.py_reader import EventDatReader
+from metavision_core.event_io import EventsIterator
+
 import logging
 logger = logging.getLogger(__name__)
 
@@ -27,7 +34,7 @@ class Window(QtWidgets.QMainWindow):
         
         ''' Set up the UI '''
         loadUi('gui/accordion_gui.ui', self)
-        self.setWindowTitle('Thread Template')
+        self.setWindowTitle('Accordion Event Browser')
 
         self.initialize_and_connect_menubar()
 
@@ -41,6 +48,33 @@ class Window(QtWidgets.QMainWindow):
         self.state.state_updated.connect(lambda string: print('Window thread received state update: ',string))
         # self.logger = SingletonLoggerObject('my_log_file.txt')
         # print('ID Window Logger: ', id(self.logger))
+
+        self.w1 = self.graphicsView.addPlot()
+
+        self.s4 = pg.ScatterPlotItem(
+                    size=10,
+                    pen=pg.mkPen(None),
+                    brush=pg.mkBrush(255, 255, 255, 20),
+                    hoverable=True,
+                    hoverSymbol='s',
+                    hoverSize=15,
+                    hoverPen=pg.mkPen('r', width=2),
+                    hoverBrush=pg.mkBrush('g'),
+                    )
+        self.n = 10000
+        self.pos = np.random.normal(size=(2, self.n), scale=1e-9)
+        self.s4.addPoints(x=self.pos[0],
+                            y=self.pos[1],
+                            # size=(np.random.random(n) * 20.).astype(int),
+                            # brush=[pg.mkBrush(x) for x in np.random.randint(0, 256, (n, 3))],
+                            data=np.arange(self.n)
+                            )
+        self.w1.addItem(self.s4)
+
+        self.clickedPen = pg.mkPen('b', width=2)
+        self.lastClicked = []    
+
+        self.s4.sigClicked.connect(self.clicked)
 
         ''' Set the thread up '''
         self.my_thread = QtCore.QThread()
@@ -78,12 +112,33 @@ class Window(QtWidgets.QMainWindow):
             pass
 
     def initialize_and_connect_menubar(self):
-        self.actionExit.triggered.connect(self.close_app)
-
+        self.actionExit_2.triggered.connect(self.close_app)
+        self.actionOpen_File.triggered.connect(self.load_dataset)
+    
     def close_app(self):
         self.__del__()
         self.close()
-        
+
+    def load_dataset(self):
+        path , _ = QtWidgets.QFileDialog.getOpenFileName(self, 'Open File')
+
+        ''' To avoid crashes, only set the cfg file when a file has been selected:'''
+        if path:
+            path = path.decode('ascii')
+            print(path)
+            logger.info(f'Main Window: Chosen File: {path}')
+            self.record_raw = RawReader(path)
+            print(record_raw)
+            self.events = self.record_raw.load_n_events(10000)
+
+    def clicked(self, plot, points):
+        for p in self.lastClicked:
+            p.resetPen()
+        print("clicked points", points)
+        for p in points:
+            p.setPen(self.clickedPen)
+        self.lastClicked = points
+          
     def print_sth(self, string):
         print(string)
 
